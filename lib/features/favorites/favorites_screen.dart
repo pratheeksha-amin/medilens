4e5e2e0a7ef703fda17ac3824ddb595ff2e18575
pratheeksha-ai <pratheeksha@example.com>
2026-07-22 19:04:56 +0530

    save change

--- lib/features/favorites/favorites_screen.dart
+++ lib/features/favorites/favorites_screen.dart
@@ -1,35 +1,93 @@
 import 'package:flutter/material.dart';
+import '../../services/medicine_storage_service.dart';
 
-class FavoritesScreen extends StatelessWidget {
+
+class FavoritesScreen extends StatefulWidget {
+
   const FavoritesScreen({super.key});
+
+
+  @override
+  State<FavoritesScreen> createState() =>
+      _FavoritesScreenState();
+
+}
+
+
+
+class _FavoritesScreenState extends State<FavoritesScreen> {
+
+  List<Map<String,dynamic>> medicines = [];
+
+
+  @override
+  void initState() {
+    super.initState();
+    loadFavorites();
+  }
+
+
+  Future<void> loadFavorites() async {
+
+    final data =
+    await MedicineStorageService.getFavourite();
+
+    setState(() {
+      medicines = data;
+    });
+
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    final favorites = [
-      "Paracetamol",
-      "Vitamin C",
-      "Cetirizine",
-    ];
 
     return Scaffold(
+
       appBar: AppBar(
         title: const Text("Favorites"),
       ),
-      body: ListView.builder(
-        itemCount: favorites.length,
-        itemBuilder: (context, index) {
+
+
+      body: medicines.isEmpty
+
+          ? const Center(
+        child: Text(
+          "No favorite medicines",
+        ),
+      )
+
+
+          : ListView.builder(
+
+        itemCount: medicines.length,
+
+        itemBuilder: (context,index){
+
           return Card(
-            margin: const EdgeInsets.all(10),
+
             child: ListTile(
+
               leading: const Icon(
                 Icons.favorite,
                 color: Colors.red,
               ),
-              title: Text(favorites[index]),
+
+              title: Text(
+                medicines[index]["name"]
+                    .toString(),
+              ),
+
             ),
+
           );
+
         },
+
       ),
+
     );
+
   }
+
 }
